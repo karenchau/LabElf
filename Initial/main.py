5791a5e878efc7e1e384d9473ccb627d497b9799
karenchau <kchau@smith.edu>
2016-01-19 12:02:37 -0500

    added 3 fields to form

--- Initial/main.py
+++ Initial/main.py
@@ -15,10 +15,19 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+import logging
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template_initial = JINJA_ENVIRONMENT.get_template('templates/index.html')
+        self.response.write(template_initial.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
